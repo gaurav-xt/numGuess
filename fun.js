c5df0d04932671c126e2f5e857e258bec92eb36d
gaurav-xt <gauravtambare7@gmail.com>
2025-06-11 17:21:31 +0530

    hello

---
+++ fun.js
@@ -0,0 +1,39 @@
+
+
+const minNum = 1;
+const maxNum = 100;
+
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1));
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running){
+
+    guess = window.prompt(`Guess the number between ${minNum} - ${maxNum}`);
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+
+    }
+    else if(guess < minNum || guess > maxNum){
+        window.alert("Please enter a valid number");
+    }
+    else{
+        attempts++;
+        if(guess < answer){
+            window.alert("Too Low! Try again");
+        }
+        else if(guess > answer){
+            window.alert("Too High! Try again")
+        }
+        else{
+            window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts`);
+            running = false;
+        }
+    }
+
+
+}
