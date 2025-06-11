f27362225c324433d8d79963aa3759fff6000c3d
Gaurav <gauravtambare7@gmail.com>
2025-06-11 17:34:09 +0530

    fun.js

--- fun.js
+++ fun.js
@@ -3,7 +3,7 @@
 const minNum = 1;
 const maxNum = 100;
 
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1));
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
 let attempts = 0;
 let guess;
